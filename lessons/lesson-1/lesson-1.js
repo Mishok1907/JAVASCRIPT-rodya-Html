8fb301fe0a483a70258bae6cb0d76d0637f0c5f4
Rodya <rodyatmb@gmail.com>
2026-09-22 20:04:44 +0300

    lesson 22 09

---
+++ lessons/lesson-1/lesson-1.js
@@ -0,0 +1,62 @@
+// private int x = 1;
+// x = "привет";
+
+let a = 1; // можно изменить
+a = false;
+a = "абру";
+
+const b = "мяу" // когда не изменяется
+const c = true // когда не изменяется
+
+// ----- //
+
+const num = 23;
+const price = 23.33;
+const title = "пять";
+const isOpen = true;
+const Image = null;
+
+
+let name; // undefined
+
+// public class User
+// {
+//      public float speedPlayer = 10f;
+//      private string name = "Вася";
+//}
+// public class Game
+// {
+//      private User player = new User();
+//      player.speedPlayer = 20f;
+//}
+
+const user = {name: "Вася", speedPlayer: 32};
+
+user.speedPlayer = 50;
+user.name = "Петя";
+
+// private int[] applesCount;
+
+const fruits = ["яблоко", "банан"];
+fruits.push("апельсин");
+
+let aa = 1;
+let bb = "1";
+
+let result = aa * bb;
+
+// if (aa !== bb || aa < bb && aa < bb) {}
+
+// Классический for
+for (let i = 0; i < 5; i++) {
+    console.log("Шаг", i);
+}
+
+// while — пока условие true
+let n = 3;
+while (n > 0) {
+    console.log(n);
+    n--;
+}
+
+
