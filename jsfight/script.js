9ca6de9ec1eef398702ede68b0933a3983193df5
Mishok1907 <mishok3286@yandex.ru>
2026-09-24 18:44:03 +0300

    comit

--- jsfight/script.js
+++ jsfight/script.js
@@ -7,7 +7,7 @@ const mag = {damegm: 10, hp hpm: 100};
 for (let h = 0; h = 1; h = 0 ) {
      warior.hpw = warior.hpw - mag.damegm;
      mag.hpm = mag.hpm - mag.damegw;
-     if (warior.hpw >= 0 ) {
-        
+     if (warior.hpw >= 0 || mag.hpm >= 0) {
+        h = 1;
      }
 }
